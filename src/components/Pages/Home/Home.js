4f933d5d90d36afa895a7c24059c59feb4a813fc
Ridoyb <ridoyb8730@gmail.com>
2022-11-08 08:01:14 +0600

    Banner added in home page

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
         <div>
-            <h1>This is home</h1>
+            <div className="hero  bg-base-100 mt-12 mb-12">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src="https://websitedemos.net/photographer-04/wp-content/uploads/sites/507/2020/03/bg-01-free-img.png" alt='' className=" hero-image rounded-lg shadow-2xl" />
+                    <div>
+                        <h4 className='text-center mt-12'>PROFESSIONAL PHOTOGRAPHER</h4>
+                        <h1 className="text-9xl font-bold title">SCOTT STEVENS</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div className="hero  bg-base-100 mt-12 mb-12">
+                <div className="hero-content flex-col-reverse lg:flex-row-reverse">
+                    <div className='text-center'>
+                        <h1 className="text-9xl font-bold title">INTERESTED IN MY SERVICES?</h1>
+                        <button className=" py-5 px-10 mt-9 btn-contact">Contact Me</button>
+                    </div>
+                    <img src="https://websitedemos.net/photographer-04/wp-content/uploads/sites/507/2019/10/services-03-free-img.png" alt='' className=" hero-image rounded-lg shadow-2xl" />
+                    
+                </div>
+            </div>
+
+
+            <div className='text-center mb-12'>
+                <h1 className="text-9xl font-bold title">SERVICES</h1>           
+            </div>
+
+
         </div>
     );
 };
---
+++ src/components/Pages/Home/ImageGallery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ImageGallery = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ImageGallery;
