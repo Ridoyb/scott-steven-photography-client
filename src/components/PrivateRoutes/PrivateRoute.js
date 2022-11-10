131e3abc41b604f5302d8715bdd323f4f3d8e3e1
Ridoyb <ridoyb8730@gmail.com>
2022-11-10 22:46:25 +0600

    private routes added

--- src/components/PrivateRoutes/PrivateRoute.js
+++ src/components/PrivateRoutes/PrivateRoute.js
@@ -3,8 +3,8 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
 
 const PrivateRoute = ({children}) => {
-    const {user, loading} = useContext(AuthContext);
-    const location = useLocation();
+        const {user, loading} = useContext(AuthContext);
+        const location = useLocation();
 
     if(loading){
         return  <button>Loading...</button>
